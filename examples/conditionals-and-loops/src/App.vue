59d2abda5889117918e95655a4883251e47c7b82
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-14 12:15:00 +0200

    Add conditionals and loop example

---
+++ examples/conditionals-and-loops/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="conditional-rendering">
+    <span v-if="seen">Now you see me</span>
+    <button @click="seen=!seen">Toggle</button>
+  </div>
+  <div id="list-rendering">
+    <ol>
+      <li v-for="todo in todos" :key="todo.text">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      seen: true,
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  }
+});
+</script>
