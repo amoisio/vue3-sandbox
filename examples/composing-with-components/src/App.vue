dfe7166866b9bf1bcad59103a579714db79ff52d
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-14 12:36:10 +0200

    Add composing with components example

---
+++ examples/composing-with-components/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="todo-list-app">
+    <ol>
+      <todo v-for="item in groceryList" :todo="item" :key="item.id"> </todo>
+    </ol>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Todo from "./Todo.vue";
+
+export default defineComponent({
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      groceryList: [
+        { id: 1, text: "test1" },
+        { id: 2, text: "test2" },
+      ],
+    };
+  },
+});
+</script>
---
+++ examples/composing-with-components/src/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+    <li>{{ todo.text }}</li>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        todo: {
+            type: Object as PropType<{ text: String }>,
+            required: true
+        }
+    }    
+})
+</script>
