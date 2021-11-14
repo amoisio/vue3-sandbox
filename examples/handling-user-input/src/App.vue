4b54d97b0d41e91cdb057e436f882dfbf482791d
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-14 12:05:12 +0200

    Add handling user input example

---
+++ examples/handling-user-input/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="event-handling">
+    <p>{{ message }}</p>
+    <button @click="reverse">Reverse Message</button>
+  </div>
+  <div id="two-way-binding">
+    <p>{{ userMessage }}</p>
+    <input type="text" v-model="userMessage" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      message: "This is a reversable message",
+      userMessage: "This is a user message"
+    };
+  },
+  methods: {
+    reverse() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('');
+    }
+  }
+});
+</script>
