0da02f95919b2f2854cad7378e2688cd613ad788
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-14 11:53:11 +0200

    Add declarative rendering example

--- examples/declarative-rendering/src/App.vue
+++ examples/declarative-rendering/src/App.vue
@@ -1,4 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Welcome to Your Vue.js + TypeScript App</h1>
+  <div id="counter">
+    Counter : {{ counter }}
+  </div>
+  <div id="bind-attribute">
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see the message!
+    </span>
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  data() {
+    return {
+      counter: 0,
+      message: "Hover message"
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++;
+    }, 1000);
+  }
+})
+</script>
