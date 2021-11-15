2782256c8cf1e0a770449415dfa736844ddd10ac
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-11-15 10:32:05 +0200

    Add class and style binding example

---
+++ examples/class-and-style-bindings/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <p :class="{ large: isLarge, red: isRed }">Welcome to Your Vue.js + TypeScript App</p>
+  <button @click="isLarge=!isLarge">Large?</button>
+  <button @click="isRed=!isRed">Red?</button>
+
+  <p :class="computedClass">This used a computed class object</p>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      isLarge: false,
+      isRed: false
+    }
+  },
+  computed: {
+    computedClass() : any {
+      return {
+        large: this.isLarge,
+        red: this.isRed
+      }
+    }
+  }
+})
+</script>
+<style scoped>
+.red {
+  color:red
+}
+
+.large {
+  font-size: x-large;
+}
+
+</style>
+
